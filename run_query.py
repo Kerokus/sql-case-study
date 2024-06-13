2ef2bc30530d590d3731729cfb568e9501ccfba7
Kerokus <levenebb@gmail.com>
2024-06-13 16:50:25 -0400

    First test

--- run_query.py
+++ run_query.py
@@ -3,14 +3,16 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
+##### SET UP ENVIRONMENT #####
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 
-# Connect to the PostgreSQL database
+##### CONNECT TO DB #####
 conn = pg2.connect(
     dbname=DB_NAME,
     user=DB_USER,
@@ -21,6 +23,19 @@ conn = pg2.connect(
 
 c = conn.cursor()
 
+
+#### INSERT YOUR QUERY ######
 query = '''
-INSERT SQL QUERY HERE
+
 '''
+
+#### RUN THE QUERY ######
+result_df = pd.read_sql(query, conn)
+
+
+#### OUTPUT REPORT ######
+output_file_csv = 'excel_reports/Q1_HR_Report.csv'
+result_df.to_csv(output_file_csv, index=False)
+
+#### CLOSE THE CONNECTION ######
+conn.close()
